400f1d19679a3d38b4b42530a6d7e3f0a494ae36
TeamHarley <132205419+TeamHarley@users.noreply.github.com>
2023-05-01 16:30:23 +0530

    Add files via upload

--- HarleyMusic/utils/decorators/__init__.py
+++ HarleyMusic/utils/decorators/__init__.py
@@ -1,1 +1,11 @@
+#
+# Copyright (C) 2021-2022 by TeamHarley@Github, < https://github.com/TeamHarley >.
+#
+# This file is part of < https://github.com/TeamHarley/Harley MusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamHarley/HarleyMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
 
+from .admins import *
+from .language import *
